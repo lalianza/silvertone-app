846830f6806a5544f02f903b5cecc3aa501e1e14
luizlianza <lianzadafranca@gmail.com>
2022-11-28 12:13:58 -0300

    front-end

--- app.py
+++ app.py
@@ -5,6 +5,38 @@ import pickle
 import io
 import numpy as np
 import tensorflow as tf
+import base64
+
+st.title("Silvertone!")
+#st.header("Record a 5 seconds audio, and receive a % ....")
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+st.sidebar.title("Welcome to Silvertone!")
+st.sidebar.image("logo2.png", use_column_width=True)
+st.sidebar.text("Contributors:")
+st.sidebar.text("Luiz Lianza")
+st.sidebar.text("Victor Sattamini ")
+st.sidebar.text("Lucas Gama")
+st.sidebar.text("Guilherme Barreto")
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('logo2.png')
 
 def preprocessing (audio):
     spectrograms = []
