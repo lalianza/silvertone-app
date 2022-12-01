5af3f1f98142ade0e20f36a9cf40006bdfdf4f97
luizlianza <lianzadafranca@gmail.com>
2022-12-01 16:42:10 -0300

    beta version 2

--- app.py
+++ app.py
@@ -8,20 +8,31 @@ import tensorflow as tf
 import base64
 import webbrowser
 import speech_recognition as sr
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from plotly.graph_objs import *
+import librosa.display
 
 
-
-
-
-st.title("Silvertone!")
-#st.header("Record a 5 seconds audio, and receive a % ....")
+st.title("Silvertone")
+st.markdown("""We created this app to be able to recognize emotion in spoken english.
+We hope you enjoy it
+To use it:
+1. Use the button below to record an audio acting out an emotion (try to keep it short, the app likes it better that way!)
+2. Let the app do its magic
+3. See if it matches! 
+4. The spectrogram tab will give you a visual representation of your audio""")
+st.subheader("Record an audio.... :")
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 </style> """, unsafe_allow_html=True)
 
-st.sidebar.title("Welcome to Silvertone!")
-st.sidebar.image("logo_new.png", use_column_width=True)
+#st.sidebar.title("Welcome to Silvertone!")
+st.sidebar.image("final_logo.png", use_column_width=True)
+st.sidebar.write("[Repository](https://github.com/vsattamini/silvertone)")
+
 st.sidebar.write("Contributors:")
 st.sidebar.write("[Luiz Lianza](https://github.com/lalianza)")
 st.sidebar.write("[Victor Sattamini](https://github.com/vsattamini)")
@@ -30,19 +41,19 @@ st.sidebar.write("[Guilherme Barreto](https://github.com/guipyc)")
 
 
 def preprocessing (audio):
-    spectrograms = []
+    mfcc = []
     X, sr = librosa.load(audio)
     X_trim = librosa.effects.trim(X,top_db=35)
-    if X_trim[0].shape[0] >= 65000:
-        X_final = X_trim[0][:65000]
+    if X_trim[0].shape[0] >= 50000:
+        X_final = X_trim[0][:50000]
         X_final = tf.convert_to_tensor(X_final).numpy()
     else:
-        zero_padding = tf.zeros([65000]-tf.shape(X_trim[0]),dtype=tf.float32)
+        zero_padding = tf.zeros([50000]-tf.shape(X_trim[0]),dtype=tf.float32)
         X_final = tf.concat([X_trim[0],zero_padding],0).numpy()
-    S = librosa.feature.melspectrogram(y=X_final, sr=sr,n_mels=128)
-    spectrograms.append(S)
-    X= np.array(spectrograms)
-    X_flat=X.reshape(X.shape[0],128*127)
+    S = librosa.feature.mfcc(y=X_final, sr=sr)
+    mfcc.append(S)
+    X= np.array(mfcc)
+    X_flat=X.reshape(X.shape[0],20*98)
     return X_flat
 
 filename = None
@@ -53,7 +64,7 @@ r = sr.Recognizer()
 if audio_bytes:
     st.audio(audio_bytes, format='audio/wav')
     X_flat = preprocessing(io.BytesIO(audio_bytes))
-    model_pickle = open("model_four_emotions_72,96.sav", "rb")
+    model_pickle = open("model_svc_5emo_64,5.sav", "rb")
     model = pickle.load(model_pickle)
     y = model.predict(X_flat)
     st.subheader(y[0])
@@ -64,6 +75,21 @@ if audio_bytes:
     except:
         text = "Sorry, can you repeat that?"
         st.subheader(text)
+    tab1, tab2 = st.tabs([" ","Spectrogram"])
+    with tab2:
+       fig, ax = plt.subplots(facecolor=(0, 0, 0,0))
+       X, sr = librosa.load((io.BytesIO(audio_bytes)))
+       #S_dB = librosa.power_to_db(y=X,sr=sr,n_mels=128, ref=np.max)
+       S = librosa.feature.melspectrogram(y=X, sr=sr, n_mels=128)
+       S_db_mel = librosa.amplitude_to_db(S, ref=np.max)
+       #mel_spec = librosa.features.melspectogram
+       img = librosa.display.specshow(S_db_mel, x_axis='time',
+                                y_axis='mel', sr=sr,
+                                fmax=8000, ax=ax)
+       fig.colorbar(img, ax=ax, format='%+2.0f dB')
+       #ax.set_facecolor(0,0,0,0)
+       ax.set(title='Mel-frequency spectrogram')
+       st.pyplot(fig)
 
 
 
